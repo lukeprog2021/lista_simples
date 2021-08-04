8d486739af48f1e0bd3a512cc43c41ce9aaad833
Lucas Souza Silva <82843331+lukeprog2021@users.noreply.github.com>
2021-08-04 23:31:28 +0000

    insere ordenado

--- main.c
+++ main.c
@@ -37,6 +37,41 @@ void imprime(Lista* l){
 	}
 }
 
+//Verifica se um elemento está na lista. Caso esteja ele retorna 1, caso não ele retorna 0
+int buscar(Lista* l, int v){
+	for(ListaNo* p = l->prim;p!=NULL;p = p->prox){
+		if(p->info == v){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void insere_ordenado(Lista* l, int v){
+	ListaNo* ant = NULL;
+	ListaNo* p = l->prim;
+	//localiza o elemento na lista e guarda a posição anterior
+	while(p!=NULL && p->info < v){
+		ant = p;
+		p = p->prox;
+	}
+
+	//cria um novo elemento na lista
+	ListaNo* novo = (ListaNo*) malloc(sizeof(ListaNo));
+	novo->info = v;//insere o valor v
+
+	//insere elemento no inicio da lista
+	if(ant == NULL){
+		novo->prox = l->prim;
+		l->prim = novo;
+	}else{
+		//insere elemento no meio da lista
+		novo -> prox = ant->prox;
+		ant->prox = novo;
+	}
+
+}
+
 
 
 
@@ -44,10 +79,14 @@ int main(void) {
 	//Gera o inicio da lista
   Lista* l = criar_lista();
 
-	//Insere os valores nos nós da lista
+	//Insere os valores nos nós da lista no inicio 
 	lista_insere(l,10);
 	lista_insere(l,15);
 
+	//Coloca os elementos de forma ordenada
+	insere_ordenado(l, 20);
+	insere_ordenado(l, 5);
+	
 	//Mostra os valores
 	imprime(l);
 
